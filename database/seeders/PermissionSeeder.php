08643c83869258a9148f2dce1988dde616747835
jessinthecloud <code@jessinthe.cloud>
2021-11-10 22:03:52 -0500

    role+permission seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -21,42 +21,128 @@ class PermissionSeeder extends Seeder
         ];
         
         // create permissions
-        Permission::create(['name' => 'do all']);
+        /*Permission::create(['name' => 'do all']);
+        
         Permission::create(['name' => 'view all']);
         Permission::create(['name' => 'create all']);
         Permission::create(['name' => 'edit all']);
-        Permission::create(['name' => 'delete all']);
-        // characters
+        Permission::create(['name' => 'delete all']);*/
+        
+    // -- characters --
         Permission::create(['name' => 'view characters']);
         Permission::create(['name' => 'create characters']);
         Permission::create(['name' => 'edit characters']);
         Permission::create(['name' => 'delete characters']);
-        // loadouts
+        // their own
+        Permission::create(['name' => 'view own characters']);
+        Permission::create(['name' => 'create own characters']);
+        Permission::create(['name' => 'edit own characters']);
+        Permission::create(['name' => 'delete own characters']);
+        // their companies'
+        Permission::create(['name' => 'view own company characters']);
+        Permission::create(['name' => 'create own company characters']);
+        Permission::create(['name' => 'edit own company characters']);
+        Permission::create(['name' => 'delete own company characters']);
+        // their factions'
+        Permission::create(['name' => 'view own faction characters']);
+        Permission::create(['name' => 'create own faction characters']);
+        Permission::create(['name' => 'edit own faction characters']);
+        Permission::create(['name' => 'delete own faction characters']);
+    // -- loadouts --
         Permission::create(['name' => 'view loadouts']);
         Permission::create(['name' => 'create loadouts']);
         Permission::create(['name' => 'edit loadouts']);
         Permission::create(['name' => 'delete loadouts']);
-        // companies
+        // their own
+        Permission::create(['name' => 'view own loadouts']);
+        Permission::create(['name' => 'create own loadouts']);
+        Permission::create(['name' => 'edit own loadouts']);
+        Permission::create(['name' => 'delete own loadouts']);
+        // their companies'
+        Permission::create(['name' => 'view own company loadouts']);
+        Permission::create(['name' => 'create own company loadouts']);
+        Permission::create(['name' => 'edit own company loadouts']);
+        Permission::create(['name' => 'delete own company loadouts']);
+        // their factions'
+        Permission::create(['name' => 'view own faction loadouts']);
+        Permission::create(['name' => 'create own faction loadouts']);
+        Permission::create(['name' => 'edit own faction loadouts']);
+        Permission::create(['name' => 'delete own faction loadouts']);
+    // -- companies --
+        // do all for their company
+        Permission::create(['name' => 'govern company']);
+        
         Permission::create(['name' => 'view companies']);
         Permission::create(['name' => 'create companies']);
         Permission::create(['name' => 'edit companies']);
         Permission::create(['name' => 'delete companies']);
-        // rosters
+        // their own
+        Permission::create(['name' => 'view own companies']);
+        Permission::create(['name' => 'create own companies']);
+        Permission::create(['name' => 'edit own companies']);
+        Permission::create(['name' => 'delete own companies']);
+        // their factions'
+        Permission::create(['name' => 'view own faction companies']);
+        Permission::create(['name' => 'create own faction companies']);
+        Permission::create(['name' => 'edit own faction companies']);
+        Permission::create(['name' => 'delete own faction companies']);
+    // -- rosters --
         Permission::create(['name' => 'view rosters']);
         Permission::create(['name' => 'create rosters']);
         Permission::create(['name' => 'edit rosters']);
         Permission::create(['name' => 'delete rosters']);
-        // users
+        // their own
+        Permission::create(['name' => 'view own rosters']);
+        Permission::create(['name' => 'create own rosters']);
+        Permission::create(['name' => 'edit own rosters']);
+        Permission::create(['name' => 'delete own rosters']);
+        // their companies'
+        Permission::create(['name' => 'view own company rosters']);
+        Permission::create(['name' => 'create own company rosters']);
+        Permission::create(['name' => 'edit own company rosters']);
+        Permission::create(['name' => 'delete own company rosters']);
+        // their factions'
+        Permission::create(['name' => 'view own faction rosters']);
+        Permission::create(['name' => 'create own faction rosters']);
+        Permission::create(['name' => 'edit own faction rosters']);
+        Permission::create(['name' => 'delete own faction rosters']);
+        
+    // -- events --
+        Permission::create(['name' => 'view events']);
+        Permission::create(['name' => 'create events']);
+        Permission::create(['name' => 'edit events']);
+        Permission::create(['name' => 'delete events']);
+        // their own
+        Permission::create(['name' => 'view own events']);
+        Permission::create(['name' => 'create own events']);
+        Permission::create(['name' => 'edit own events']);
+        Permission::create(['name' => 'delete own events']);
+        // their companies'
+        Permission::create(['name' => 'view own company events']);
+        Permission::create(['name' => 'create own company events']);
+        Permission::create(['name' => 'edit own company events']);
+        Permission::create(['name' => 'delete own company events']);
+        // their factions'
+        Permission::create(['name' => 'view own faction events']);
+        Permission::create(['name' => 'create own faction events']);
+        Permission::create(['name' => 'edit own faction events']);
+        Permission::create(['name' => 'delete own faction events']);
+        
+    // -- users --
         Permission::create(['name' => 'view users']);
         Permission::create(['name' => 'create users']);
         Permission::create(['name' => 'edit users']);
         Permission::create(['name' => 'delete users']);
+
+        Permission::create(['name' => 'view own faction users']);
+        Permission::create(['name' => 'view own company users']);
+        Permission::create(['name' => 'edit users cosmetic info']);
         
         // ownership
-        Permission::create(['name' => 'of all']);
-        Permission::create(['name' => 'of their company']);
-        Permission::create(['name' => 'of their faction']);
+        /*Permission::create(['name' => 'of all']);
         Permission::create(['name' => 'of their own']);
+        Permission::create(['name' => 'of their company']);
+        Permission::create(['name' => 'of their faction']);*/
 
         /*// create permissions
         Permission::create(['name' => 'do all']);
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,15 +15,141 @@ class RoleSeeder extends Seeder
 
         // super admin gets all permissions via Gate::before rule; see AuthServiceProvider
         $role1 = Role::create(['name' => 'super-admin']);
-        $role1->givePermissionTo('do all', 'for all');
+        $role1->givePermissionTo(
+            'view characters', 'create characters', 'edit characters', 'delete characters', 
+            'view loadouts', 'create loadouts', 'edit loadouts', 'delete loadouts',
+            'view companies', 'create companies', 'edit companies', 'delete companies', 
+            'view rosters', 'create rosters', 'edit rosters', 'delete rosters',
+            'view events', 'create events', 'edit events', 'delete events',
+            'view positions', 'create positions', 'edit positions', 'delete positions',
+            'view users', 'create users', 'edit users', 'delete users'
+        );
 
-        $role2 = Role::create(['name' => 'admin']);
-        $role2->givePermissionTo('view characters', 'create characters', 'edit characters', 'view loadouts', 'create loadouts', 'edit loadouts', 'view companies', 'create companies', 'edit companies', 'view rosters', 'create rosters', 'edit rosters', 'view users', 'create users', 'edit users');
+        $admin = Role::create(['name' => 'admin']);
+        $admin->givePermissionTo(
+            'view characters', 'create characters', 'edit characters', 
+            'view loadouts', 'create loadouts', 'edit loadouts', 
+            'view companies', 'create companies', 'edit companies', 
+            'view rosters', 'create rosters', 'edit rosters',
+            'view events', 'create events', 'edit events',
+            'view positions', 'create positions', 'edit positions',
+            'view users', 'edit users cosmetic info'
+        );
 
-        $role3 = Role::create(['name' => 'governor']);
-        $role3 = Role::create(['name' => 'officer']);
-        $role3 = Role::create(['name' => 'settler']);
-        $role3 = Role::create(['name' => 'player']);
+        $governor = Role::create(['name' => 'governor']);
+        $governor->givePermissionTo(
+            
+            'govern company',
+            
+        // -- own permissions
+            'view own characters',
+            'create own characters',
+            'edit own characters',
+            'delete own characters',
+            
+            'view own loadouts',
+            'create own loadouts',
+            'edit own loadouts',
+            'delete own loadouts',
+            
+            'view own companies',
+            'edit own companies',
+            
+            'view own faction companies',
+            'view own rosters',
+            'view own company rosters',
+
+        // -- company permissions
+            'view own company users',
+            'view own company rosters',
+            'create own company rosters',
+            'edit own company rosters',
+            'view own company characters',
+            'view own company loadouts',
+            'view own company events', 'create own company events', 'edit own company events',
+            'view own company positions', 'create own company positions', 'edit own company positions',
+
+        // -- faction permissions
+            'view own faction companies',
+            'view own faction rosters',
+            'view own faction events',
+            'view own faction positions',
+            'view own faction users',
+            'view own faction characters',
+            'view own faction loadouts',
+        );
+        
+        $officer = Role::create(['name' => 'officer']);
+        $officer->givePermissionTo(
+        
+        // -- own permissions
+            'view own characters',
+            'create own characters',
+            'edit own characters',
+            'delete own characters',
+    
+            'view own loadouts',
+            'create own loadouts',
+            'edit own loadouts',
+            'delete own loadouts',
+    
+            'view own companies',
+            'view own rosters',
+            
+            'view own company rosters', 'create own company rosters', 'edit own company rosters',
+            'view own company events', 'create own company events', 'edit own company events',
+            'view own company positions', 'create own company positions', 'edit own company positions',
+            
+            'view own faction companies',
+    
+        // -- company permissions
+            'view own company users',
+            'view own company rosters',
+            'create own company rosters',
+            'edit own company rosters',
+            'view own company characters',
+            'view own company loadouts',
+    
+        // -- faction permissions
+
+        );
+        
+        $settler = Role::create(['name' => 'settler']);
+        $settler->givePermissionTo(
+            // -- own permissions
+            'view own characters',
+            'create own characters',
+            'edit own characters',
+            'delete own characters',
+    
+            'view own loadouts',
+            'create own loadouts',
+            'edit own loadouts',
+            'delete own loadouts',
+    
+            'view own companies',
+    
+            'view own faction companies',
+            'view own company rosters',
+            'view own rosters',
+        );
+        
+        $user = Role::create(['name' => 'user']);
+        $user->givePermissionTo(
+            // -- own permissions
+            'view own characters',
+            'create own characters',
+            'edit own characters',
+            'delete own characters',
+    
+            'view own loadouts',
+            'create own loadouts',
+            'edit own loadouts',
+            'delete own loadouts',
+    
+            'view own companies',
+            'view own rosters',
+        );
         
         /*DB::table('roles')->insert([
             [
