06b24ab50482cc3d03017d0df5f83300b26fd576
jessinthecloud <code@jessinthe.cloud>
2021-12-29 22:04:38 -0500

    add discord name for sheet import

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('discord_name')->nullable()->unique();
             $table->string('nickname')->nullable();
             $table->string('email')->nullable()->unique();
             $table->string('password')->nullable();
