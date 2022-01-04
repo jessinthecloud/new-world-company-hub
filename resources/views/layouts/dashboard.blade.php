d972963ff327a9fab94ef793548c6fff2c861416
jessinthecloud <code@jessinthe.cloud>
2022-01-03 19:01:10 -0500

    add current character to dashboard heading

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,7 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ (isset($title) ? $title.' | ' : '') . __('Dashboard') }} 
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex justify-between">
+            <span>{{ (isset($title) ? $title.' | ' : '') . __('Dashboard') }}</span> 
+            @if( session('character') !== null ) 
+                <span id="character-heading" class="text-gray-600">
+                    {{ session('character')->company->name }} 
+                    <span class="text-gray-400">/</span> 
+                    {{ session('character')->name }}
+                </span> 
+            @endif
         </h2>
     </x-slot>
     
