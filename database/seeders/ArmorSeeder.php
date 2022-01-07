97d1956e24ec2b9a7ccf6b538d7aad9cc8047e3f
jessinthecloud <code@jessinthe.cloud>
2022-01-06 22:40:41 -0500

    loop dir for armor seeder

--- database/seeders/ArmorSeeder.php
+++ database/seeders/ArmorSeeder.php
@@ -5,36 +5,53 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveTreeIterator;
 
 class ArmorSeeder extends Seeder
 {
     public function run()
     {
-        $data = json_decode(file_get_contents(__DIR__.'/../../storage/app/json/armor/armor-1.json'));
-    
+        $dir = __DIR__ . '/../../storage/app/json/armor';
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator( $dir, RecursiveDirectoryIterator::SKIP_DOTS )
+        );
+        foreach ( $files as $file ) {
+//    dump($file->getPathname());
+
+            $data = json_decode( file_get_contents( $file->getPathname() ) );
+
 //        dump(collect($data->subjects->data)->pluck('attributes')); // armor data
 //        dump(collect($data->subjects->data)->pluck('attributes')->pluck('item_class_en')->unique()->all()); // armor type
-        
-        $armors = collect($data->subjects->data)->pluck('attributes');
-        
-        $insert = [];
-        foreach($armors as $armor){
-            $insert []= [
-                'name' => $armor->name,
-                'long_name' => $armor->name_with_affixes,
-                'slug' => $armor->slug, //Str::slug($armor->name),
-                'description' => $armor->description,
-                'armor_type' => Str::before($armor->item_class_en, '('),
-                'weight_class' => Str::beforeLast(Str::after($armor->item_class_en, '('), ')') ?? null,
-                'tier' => $armor->tier,
-                'rarity' => $armor->rarity,
-                'gear_score_override' => $armor->gear_score_override ?? null,
-                'min_gear_score' => $armor->min_gear_score ?? null,
-                'max_gear_score' => $armor->max_gear_score ?? null,
-                'cdn_asset_path' => $armor->cdn_asset_path ?? null,
-            ];
+
+            $armors = collect( $data->subjects->data )->pluck( 'attributes' );
+
+            $insert = [];
+            foreach ( $armors as $armor ) {
+                // separate weight class
+                $weight_class = Str::contains($armor->item_class_en, '(') ? Str::beforeLast( Str::after( $armor->item_class_en, '(' ), ')' ) : null;
+                // create unique slug
+                $slug = $armor->name . (!empty($armor->rarity) ? ' '.$armor->rarity : '') . (!empty($armor->tier) ? ' t'.$armor->tier : '') . (!empty($weight_class) ? ' '.$weight_class : '');
+                $slug = Str::slug($slug);
+            
+                $insert [] = [
+                    'name'                => $armor->name,
+                    'long_name'           => $armor->name_with_affixes,
+                    'slug'                => $slug, //$armor->slug,
+                    'description'         => $armor->description,
+                    'armor_type'          => Str::before( $armor->item_class_en, '(' ),
+                    'weight_class'        => $weight_class,
+                    'tier'                => $armor->tier,
+                    'rarity'              => $armor->rarity,
+                    'gear_score_override' => $armor->gear_score_override ?? null,
+                    'min_gear_score'      => $armor->min_gear_score ?? null,
+                    'max_gear_score'      => $armor->max_gear_score ?? null,
+                    'cdn_asset_path'      => $armor->cdn_asset_path ?? null,
+                ];
+            }
+
+            DB::table( 'armors' )->upsert( $insert, ['slug'] );
         }
-        
-        DB::table('armors')->insert( $insert );
     }
 }
