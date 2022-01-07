e1fcc0be364062ea1ed3310bde40323e98fa2e3b
jessinthecloud <code@jessinthe.cloud>
2022-01-06 21:11:43 -0500

    more enums and add backings to enums

---
+++ app/Enums/ArmorType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum ArmorType : string
+{
+    case CHEST = "Chest";
+    case HELMET = "Helmet";
+    case GLOVES = "Gloves";
+    case PANTS = "Pants";
+    case SHOES = "Shoes";
+    case AMULET = "Amulet";
+    case EARRING = "Earring";
+    case RING = "Ring";
+    case BAG = "Bag";
+}
---
+++ app/Enums/WeaponType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Enums;
+
+enum WeaponType : string
+{
+    case BOW = "Bow";
+    case FIRE_STAFF = "Fire Staff";
+    case GREAT_AXE = "Great Axe";
+    case HATCHET = "Hatchet";
+    case ICE_GAUNTLET = "Ice Gauntlet";
+    case LIFE_STAFF = "Life Staff";
+    case MUSKET = "Musket";
+    case RAPIER = "Rapier";
+    case SHIELD = "Shield";
+    case SPEAR = "Spear";
+    case SWORD = "Sword";
+    case VOID_GAUNTLET = "Void Gauntlet";
+    case WAR_HAMMER = "War Hammer";
+}
