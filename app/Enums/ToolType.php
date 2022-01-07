a19553fb7d6643cc10530e16adab4cad7e4c83a8
jessinthecloud <code@jessinthe.cloud>
2022-01-06 21:17:08 -0500

    add tooltype enum

---
+++ app/Enums/ToolType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum ToolType : string
+{
+    case LOGGING_AXE = "Logging Axe";
+    case SKINNING_KNIFE = "Skinning Knife";
+    case SICKLE = "Sickle";
+    case PICKAXE = "Pickaxe";
+    case FISHING_POLE = "Fishing Pole";
+    case AZOTH_STAFF = "Azoth Staff"; 
+}
