a244aa95894f441556a6a3f844f5153fabf55770
jessinthecloud <code@jessinthe.cloud>
2022-01-15 12:49:11 -0500

    add colors to rarity

--- app/Enums/Rarity.php
+++ app/Enums/Rarity.php
@@ -14,4 +14,16 @@ enum Rarity : string
     case R3 = "Rare";
     case R4 = "Epic";
     case R5 = "Legendary";
+
+    public function color() : string
+    {
+        return match($this) 
+        {
+            Rarity::R1 => '#575750',
+            Rarity::R2 => '#57A848',
+            Rarity::R3 => '#217897',
+            Rarity::R4 => '#B257B0',
+            Rarity::R5 => '#D9852F',
+        };
+    }
 }
