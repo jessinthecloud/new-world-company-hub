39f030d2eccdca7dffb27765144b931515ee259b
jessinthecloud <code@jessinthe.cloud>
2022-01-16 22:22:38 -0500

    fix js bug

--- resources/views/components/layouts/html-footer.blade.php
+++ resources/views/components/layouts/html-footer.blade.php
@@ -8,7 +8,7 @@
     @stack('scripts')
     
     @livewireScripts
-<style>
+{{--<style>
         .tooltip {
             background-color: #333;
             border-radius: 5px;
@@ -26,16 +26,16 @@
         .tooltip div {
             position:relative;
         }
-    </style>
+    </style>--}}
     <script>
-        $(document).ready(function(){
-            $('#add-attr').click(function(){
+        $(document).ready(function() {
+            $('#add-attr').click(function () {
                 $('#appended-attrs').append($('#attr-wrapper').clone());
             });
-            $('#add-perk').click(function(){
+            $('#add-perk').click(function () {
                 $('#appended-perks').append($('#perks-wrapper').clone());
             });
-                        
+
             /*$('tr').hover(function(){
                 let type = $(this).children('td').first().next().text().toLowerCase().trim();
                 let slug = $(this).children('td').last().text().trim();
@@ -76,40 +76,40 @@ console.log($this.children('td').first().html());
             }, function(){
                 $this.children('td').first().css('position','');
                 $('.tooltip-wrapper').remove();
-            });
+            });// end tr hover*/
             
-        }); // end tr hover*/
-        
-        function getName(item){
-            return item.name;
-        }
-        
-        async function getItems(type) {
+        });
 
-console.log('fetching all: ' + type);
+                    function getName(item){
+                        return item.name;
+                    }
+                    
+                    async function getItems(type) {
+            
+            console.log('fetching all: ' + type);
+            
+                        let response = await fetch('/' + type);
+                        let json = await response.json();
+            console.log('response: ',json);
+                        return await json;
+                    }
+                    
+                    async function getItem(type, slug) {
+                    
+            console.log('fetching: '+type+': '+slug);        
+                        
+                        let response = await fetch('/'+type+'/'+slug);
+            console.log('response: '+response);            
+                        return response.html();
 
-            let response = await fetch('/' + type);
-            let json = await response.json();
-console.log('response: ',json);
-            return await json;
-        }
-        
-        async function getItem(type, slug) {
-        
-console.log('fetching: '+type+': '+slug);        
-            
-            let response = await fetch('/'+type+'/'+slug);
-console.log('response: '+response);            
-            return response.html();
-            
-            
+
             /*let perks = weapon.data.perks;
             let perkNames = [...new Set(perks.map(getName))];
             // let perkName = (perks, name) => perks.map(x=>x[name]);
 console.log('weapon response:',weapon.data, 'perks:',perks, 'perk names:',perkNames);
             
             return perkNames.join(', ');*/
-        }
+            }
     </script>
 
 </body>
