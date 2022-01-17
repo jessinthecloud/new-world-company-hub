9f59e44b84d80f5769816f34d43da04d6b35ed00
jessinthecloud <git@github.com>
2022-01-17 17:04:48 -0500

    allow multiple select on select component

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -1,12 +1,13 @@
-@props(['values', 'disabled' => false, 'required'=>false, 'autofocus'=>false])
+@props(['values', 'disabled' => false, 'required'=>false, 'autofocus'=>false, 'multiple'=>false])
 
 <select
     {!! $attributes->merge([
         'class' => 'rounded-md shadow-sm border-gray-300 pr-8 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
     ]) !!}
-    {{ $disabled ? 'disabled' : '' }}
-    {{ $required ? 'aria-required="true"' : '' }}
-    {{ $autofocus ? 'autofocus' : '' }} 
+    {{ $disabled ? ' disabled' : '' }}
+    {{ $required ? ' aria-required="true"' : '' }}
+    {{ $autofocus ? ' autofocus' : '' }} 
+    {{ $multiple ? ' multiple' : '' }} 
 >
     {{ $slot ?? '' }}
     
