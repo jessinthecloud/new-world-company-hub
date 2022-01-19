639c35a4901455d32a1c0eaebc639278130e6b6a
jessinthecloud <code@jessinthe.cloud>
2022-01-18 20:37:06 -0500

    add traits to attributetype

--- app/Enums/AttributeType.php
+++ app/Enums/AttributeType.php
@@ -2,8 +2,13 @@
 
 namespace App\Enums;
 
+use App\Traits\HasClassConstants;
+use App\Traits\IsEnum;
+
 enum AttributeType : string
 {
+    use HasClassConstants, IsEnum;
+    
     case CON = "Constitution";
     case DEX = "Dexterity";
     case FOC = "Focus";
