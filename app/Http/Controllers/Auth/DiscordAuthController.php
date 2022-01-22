9b58b12dc29a1d97a6f0b1332e3ffc98f8a2f83a
jessinthecloud <code@jessinthe.cloud>
2022-01-22 18:11:47 -0500

    use discord role to grant app role

--- app/Http/Controllers/Auth/DiscordAuthController.php
+++ app/Http/Controllers/Auth/DiscordAuthController.php
@@ -73,7 +73,9 @@ class DiscordAuthController extends Controller
     public function callback(Request $request)
     {
         try {
-            $discordUser = Socialite::driver( 'discord' )->user();
+            $discordUser = Cache::remember('user_info', 900, function() {
+                return Socialite::driver( 'discord' )->user();
+            });
             // find or create eloquent user with this discord name
             $user = User::where('discord_name',  $discordUser->nickname)->get();
 //dd($discordUser);          
@@ -118,56 +120,45 @@ class DiscordAuthController extends Controller
                     'expires_in' => $discordUser->expiresIn,
                 ]
             );
-            
-            // TODO: read role from server
-//            if(empty($user->getRoleNames()->all())){
 
-                // assign users their discord role
-                // get their roles for the guild
-                $discord_user_info = Cache::remember('user_guild_info', 600, 
-                    Http::withHeaders([
-                        "Authorization" => "Bearer ".$discordUser->token
-                    ])
-                    ->acceptJson()
-                    ->get("https://discord.com/api/users/@me/guilds/895006799319666718/member")
-                    ->json()
-                )
-                ;
-dump($discord_user_info);                
-                // match role(s) to the ones we have
-                foreach($discord_user_info['roles'] as $discord_role){
-dump($discord_role);
-                    $role_id = DB::table('discord_roles')
-                        ->select('role_id')
-                        ->where('company_id', '=', 1)
-                        ->where('id', '=', $discord_role)
-                        ->first();
-dump(
-DB::table('discord_roles')
-->select('role_id')
-->where('company_id', '=', 1)
-->where('id', '=', $discord_role)->toSql(),
-'role id: '.$role_id
-);                        
-                    $role = Role::where('id', '=', $role_id)->first();
-dd($role);
-                    if(!empty($role)){
-                        $user->assignRole($role);
-                    }
-                    // TODO: also assign character rank?
+            // assign users their discord role
+            // get their roles for the discord guild
+            $discord_user_info = Cache::remember('user_'.$user->id.'_guild_info', 900, 
+                function() use($discordUser) {
+                    return Http::withHeaders([
+                           "Authorization" => "Bearer " . $discordUser->token
+                        ])
+                        ->acceptJson()
+                        ->get( "https://discord.com/api/users/@me/guilds/895006799319666718/member" )
+                        ->json()
+                        ;
+            });
+//dump($discord_user_info);                
+            // match role(s) to the ones we have
+            foreach($discord_user_info['roles'] as $discord_role_id){
+
+                $dr = DB::table('discord_roles')
+                    ->select('role_id')
+                    ->where('company_id', '=', 1)
+                    ->where('id', '=', intval($discord_role_id))
+                    ->first();
                     
+                $role_id = $dr->role_id;
+                        
+                $role = Role::where('id', '=', $role_id)->first();
+
+                if(!empty($role)){
+                    $user->assignRole($role);
                 }
-                
-//                dd($response->json());
-//                $user->assignRole('settler');
-//            }
+                // TODO: also assign character rank?
+            } // end each discord role
 
             if ($discordUser->user['verified'] && $user->markEmailAsVerified()) {
                 event(new Verified($user));
             }
 
             Auth::login($user, true);
-            
+
             // send to character choice page to set character for this login
             return redirect(route('characters.choose', ['action'=>'login']));
            
