d80448a65b71ffa3543bc3d5998035d7d17661f3
jessinthecloud <code@jessinthe.cloud>
2022-02-02 21:45:04 -0500

    add loadout relations

--- app/Models/Characters/Loadout.php
+++ app/Models/Characters/Loadout.php
@@ -2,7 +2,10 @@
 
 namespace App\Models\Characters;
 
+use App\Models\Companies\Company;
 use App\Models\Items\BaseWeapon;
+use App\Models\Items\InventoryItem;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,14 +19,47 @@ class Loadout extends Model
     {
         return $this->belongsTo(Character::class);
     }
+    
+    public function user()
+    {
+        return $this->hasOneThrough(User::class, Character::class);
+    }
+
+    public function company(  )
+    {
+        return $this->hasOneThrough(Company::class, Character::class);
+    }
 
     public function main()
     {
-        return $this->belongsTo( BaseWeapon::class, 'main_hand_id');
+        return $this->belongsTo( InventoryItem::class, 'main_hand_id');
     }
 
     public function offhand()
     {
-        return $this->belongsTo( BaseWeapon::class, 'offhand_id');
+        return $this->belongsTo( InventoryItem::class, 'offhand_id');
+    }
+    
+    public function helmet()
+    {
+        return $this->belongsTo( InventoryItem::class, 'head_id');
+    }
+    
+// -- SCOPES 
+
+// -- MISC
+    public static function asArrayForDropDown()
+    {
+        return static::with('character','main','offhand')
+        ->orderBy('name')
+        ->orderBy('weight')
+        ->get()
+        ->mapWithKeys(function($loadout){
+            return [
+                $loadout->id => $loadout->name ?? ''
+                    . ' (Main: '.$loadout->main->name
+                    .' -- Offhand: '.$loadout->off?->name.')'
+            ];
+        })->all();
     }
 }
