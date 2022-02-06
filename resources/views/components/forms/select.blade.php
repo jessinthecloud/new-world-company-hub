b502e89c646ef41d86671d3e52e197f64fbd44a8
jessinthecloud <code@jessinthe.cloud>
2022-02-06 14:26:53 -0500

    add error border ro rarity, truncate long form error list

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -1,13 +1,15 @@
 @props(['values', 'disabled' => false, 'required'=>false, 'autofocus'=>false, 'multiple'=>false])
 
 <select
-    {!! $attributes->merge([
-        'class' => 'rounded-md shadow-sm border-gray-300 pr-8 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
-    ]) !!}
+    {!! $attributes
+        ->class(['rounded-md shadow-sm border-gray-300 pr-8 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
+        // only include this class if there is an error
+        'has-error' => ($errors->has($attributes->get('name')) || $errors->has(Str::beforeLast($attributes->get('name'), '[')))
+    ])->merge() !!}
     {{ $disabled ? ' disabled' : '' }}
     {{ $required ? ' aria-required="true"' : '' }}
     {{ $autofocus ? ' autofocus' : '' }} 
-    {{ $multiple ? ' multiple' : '' }} 
+    {{ $multiple ? ' multiple' : '' }}
 >
     {{ $slot ?? '' }}
     
