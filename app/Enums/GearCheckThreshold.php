e8f718616ea960741324425dbc0725d936c0a0e7
jessinthecloud <code@jessinthe.cloud>
2022-02-09 22:05:53 -0500

    fix score checks

--- app/Enums/GearCheckThreshold.php
+++ app/Enums/GearCheckThreshold.php
@@ -18,6 +18,7 @@ enum GearCheckThreshold: int
      * 610-619 Good
      * 620-625 Excellent
      */
+    case TooLow = 0;    // >= 545 <= 595
     case VeryLow = 545;    // >= 545 <= 595
     case Low = 596;        // >= 596 <= 599
     case Acceptable = 600; // >= 600 <= 609
@@ -35,16 +36,40 @@ enum GearCheckThreshold: int
     {
         return match ( true ) {
             // Very Low
-            $score <= GearCheckThreshold::Low && $score >= GearCheckThreshold::VeryLow => '#575750',
+            $score < GearCheckThreshold::Low->value && $score >= GearCheckThreshold::VeryLow->value => '#575750',
             // Low
-            $score <= GearCheckThreshold::Acceptable && $score >= GearCheckThreshold::Low => '#57A848',
+            $score < GearCheckThreshold::Acceptable->value && $score >= GearCheckThreshold::Low->value => '#57A848',
             // Acceptable
-            $score <= GearCheckThreshold::Good && $score >= GearCheckThreshold::Acceptable => '#217897',
+            $score < GearCheckThreshold::Good->value && $score >= GearCheckThreshold::Acceptable->value => '#217897',
             // Good
-            $score <= GearCheckThreshold::Excellent && $score >= GearCheckThreshold::Good => '#B257B0',
+            $score < GearCheckThreshold::Excellent->value && $score >= GearCheckThreshold::Good->value => '#B257B0',
             // Excellent
-            $score >= GearCheckThreshold::Excellent => '#FFA837',
+            $score >= GearCheckThreshold::Excellent->value => '#FFA837',
             default => '#FF0000', // Fail
+        };
+    }
+    
+    /**
+     * Determine threshold name based on gear score
+     *
+     * @param $score
+     *
+     * @return string
+     */
+    public static function getName( $score ) : string
+    {
+        return match ( true ) {
+            // Very Low
+            $score < GearCheckThreshold::Low->value && $score >= GearCheckThreshold::VeryLow->value => 'VeryLow',
+            // Low
+            $score < GearCheckThreshold::Acceptable->value && $score >= GearCheckThreshold::Low->value => 'Low',
+            // Acceptable
+            $score < GearCheckThreshold::Good->value && $score >= GearCheckThreshold::Acceptable->value => 'Acceptable',
+            // Good
+            $score < GearCheckThreshold::Excellent->value && $score >= GearCheckThreshold::Good->value => 'Good',
+            // Excellent
+            $score >= GearCheckThreshold::Excellent->value => 'Excellent',
+            default => 'TooLow', // Fail
         };
     }
 
