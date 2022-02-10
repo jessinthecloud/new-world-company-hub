dfb97348a658713a3e33299a88caba1ba568bea3
jessinthecloud <code@jessinthe.cloud>
2022-02-10 18:32:42 -0500

    switch loadout page to table view

--- resources/views/components/game-data/loadout-details.blade.php
+++ resources/views/components/game-data/loadout-details.blade.php
@@ -1,67 +1,45 @@
-@props(['equipment'])
+    @props(['equipment'])
 
-<div class="item-wrapper w-full mx-auto flex flex-wrap border border-{{ $equipment['rarity'] }}-300">
-    <div class="item-heading w-full flex flex-wrap gap-4 items-center bg-{{$equipment['rarity']}}-100 p-4 text-gray-800 {{--text-shadow--}}
-    lg:flex-nowrap
-    lg:text-lg
-    "> 
+<tr class="item-wrapper border border-{{ $equipment['rarity'] }}-100 text-gray-800 bg-{{$equipment['rarity']}}-50">
+   
         {{--@isset($equipment['equippableItem']->base?->icon)
             <div class="item-icon w-12 h-12 border border-{{ $equipment['rarity'] }}-500 shadow-md">
                 <img src="{{ $equipment['equippableItem']->base->icon }}" alt="{{ $equipment['equippableItem']->name }}">
             </div>
         @endisset--}}
-        <div class="item-gear-score font-bold mx-4">
-            @if(!empty($equipment['gear_check_status']))
-                <x-utils.icons.checkmark/>
-            @else
-                <x-utils.icons.xmark class="text-toolow-700"/>
-            @endif
-        </div>
-        
-        <div class="item-gear-score font-bold mx-4">
-            {{ $equipment['equippableItem']->gear_score }}
-        </div>
-        <div class="item-name">
-            {{ $equipment['equippableItem']->name }}
-        </div>
-        @isset($equipment['equippableItem']->weight_class)
-            <div class="item-weight-class italic mr-2">
-                {{ $equipment['equippableItem']->weight_class }}
-            </div>
+    {{-- score --}}
+    <td class="p-2 item-gear-score font-bold">
+        {{ $equipment['equippableItem']->gear_score }}
+    </td>
+    {{-- name --}}
+    <td class="p-2 item-name">
+        {{ $equipment['equippableItem']->name }}
+    </td>
+    {{-- attributes --}}
+    <td class="p-2 ">
+        @if(!empty($equipment['attributes_list']))
+            {!! $equipment['attributes_list'] !!}
+        @endif
+    </td>
+    {{-- type --}}
+    <td class="p-2 item-type">
+        {{ $equipment['equippableItem']->type }}
+    </td>
+    {{-- weight class --}}
+    <td class="p-2 item-weight-class italic">
+    @isset($equipment['equippableItem']->weight_class)
+        {{ $equipment['equippableItem']->weight_class }}
         @endisset
-        <div class="item-type mr-6">
-            {{ $equipment['equippableItem']->type }}
-        </div>
-        <div class="item-rarity font-bold">
-            {{ $equipment['equippableItem']->rarity }}
-        </div>
-    </div>
-    @if(!empty($equipment['perks_list']) && !empty($equipment['attributes_list']))
-        <div class="attr-perk w-full flex flex-wrap bg-{{$equipment['rarity']}}-50 ">
-            <div class="spacer w-24 h-6"></div>
-            @if(!empty($equipment['perks_list']))
-                <div class="w-full py-2 px-4 lg:w-auto lg:py-4">
-                    <span class="font-bold">Perks:</span> {{ $equipment['perks_list'] }}
-                </div>
-            @endif
-            
-            @if(!empty($equipment['attributes_list']))
-                <div class="w-full py-2 px-4 lg:w-auto lg:py-4">
-                    <span class="font-bold">Attributes:</span>
-                    {{ $equipment['attributes_list'] }}
-                </div>
-            @endif
-        </div>
-    @endif
+    </td>
+    {{-- perks --}}
+    <td class="p-2 ">
+        @if(!empty($equipment['perks_list']))
+            {!! $equipment['perks_list'] !!}
+        @endif
+    </td>
+    {{-- rarity --}}
+    <td class="p-2 item-rarity font-bold">
+        {{ $equipment['equippableItem']->rarity }}
+    </td>
     
-    @isset($emptySlots)
-        <div class=" py-2 px-4 lg:w-1/2 lg:py-4">
-            <h3 class="w-full">Open Slots</h3>
-            <ul class="mt-1 ml-2">
-                @while(--$emptySlots >= 0)
-                    <li class="mb-1">Unused Slot</li>
-                @endwhile
-            </ul>
-        </div>
-    @endisset
-</div>
+</tr>
--- resources/views/loadouts/show.blade.php
+++ resources/views/loadouts/show.blade.php
@@ -1,21 +1,26 @@
 <x-app-layout>
     <x-slot name="title">
-        Loadout / {{ $character_name }}
+        <strong class="ml-2">{{ $gear_score }}</strong> / Loadout / {{ $character_name }}
     </x-slot>
     
-    <div class="py-12">
+    <div class="py-6">
         <div id="wrapper-inner" class="max-w-7xl mx-auto flex flex-wrap sm:px-6 lg:px-8">
-            <h2 class="mb-4 flex flex-wrap items-center lg:flex-nowrap">
-                Overall Gear Score: <strong class="ml-2">{{ $gear_score }}</strong>
-                @if(!empty($loadout_check))
-                    <x-utils.icons.checkmark class="ml-2 text-acceptable-800"/>
-                @else
-                    <x-utils.icons.xmark class="ml-2 text-toolow-700"/>
-                @endif
-            </h2>
-            @foreach($equipment_slot as $equipment)
-                <x-game-data.loadout-details :equipment="$equipment" />
-            @endforeach
+            <table class="w-full text-sm">
+                <tr><td class="pt-4 pb-2 text-lg font-bold">Weapons</td></tr>
+                @foreach($weapon_slot as $equipment)
+                    <x-game-data.loadout-details :equipment="$equipment" />
+                @endforeach
+            
+                <tr><td class="pt-4 pb-2 text-lg font-bold">Armor</td></tr>
+                @foreach($armor_slot as $equipment)
+                    <x-game-data.loadout-details :equipment="$equipment" />
+                @endforeach
+            
+                <tr><td class="pt-4 pb-2 text-lg font-bold">Jewelry</td></tr>
+                @foreach($jewelry_slot as $equipment)
+                    <x-game-data.loadout-details :equipment="$equipment" />
+                @endforeach
+            </table>
         </div>
     </div>
 
