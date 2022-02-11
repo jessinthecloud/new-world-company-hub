9693b2ce4cae8985910781e056e5f8b86c21ed65
jessinthecloud <code@jessinthe.cloud>
2022-02-10 19:21:49 -0500

    add route check to gated button link

--- resources/views/components/dashboard/gated-button.blade.php
+++ resources/views/components/dashboard/gated-button.blade.php
@@ -1,11 +1,13 @@
-@props(['route', 'can',])
-
-@can(...$can)
-    <x-button-link href="{{ $route }}" 
-        {{ $attributes->merge([
-            'class' => 'mr-1 mb-2'
-        ]) }}
-    >
-        {{ empty($slot->toHtml()) ? 'View' : $slot }}
-    </x-button-link> 
-@endcan
+@props(['route', 'can', 'routeName'=>null])
+{{-- allow not set for backward compat --}}
+@if(!isset($routeName) || Route::has($routeName))
+    @can(...$can)
+        <x-button-link href="{{ $route }}" 
+            {{ $attributes->merge([
+                'class' => 'mr-1 mb-2'
+            ]) }}
+        >
+            {{ empty($slot->toHtml()) ? 'View' : $slot }}
+        </x-button-link> 
+    @endcan
+@endif
