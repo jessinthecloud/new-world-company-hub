02f9599c18699c6f7f6d15f73792306dcdefb03e
jessinthecloud <code@jessinthe.cloud>
2022-02-12 16:29:23 -0500

    add loadout middleware

---
+++ app/Http/Middleware/SetSessionLoadout.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Characters\Character;
+use App\Services\DiscordService;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SetSessionLoadout
+{
+    public function __construct(protected DiscordService $discordService) { }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+//dump( $request->user(), $request->session()->get('team_id'));
+        
+        if(empty($request->user())){
+            redirect(route('login'));
+        }
+        
+        if(!empty($request->user()->character()->loadout)){
+            return $next($request);
+        }
+        
+        // create a loadout
+        return redirect(route('loadouts.login.create'));
+    }
+}
