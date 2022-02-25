a6116b2c307ebf32dec2ebfc2ccbc662e6084dbd
jessinthecloud <code@jessinthe.cloud>
2022-02-25 11:57:07 -0500

    add warboard role+permission and policy

--- app/Policies/Events/WarBoardPolicy.php
+++ app/Policies/Events/WarBoardPolicy.php
@@ -12,41 +12,99 @@ class WarBoardPolicy
 
     public function __construct()
     {
-        return true;
+        
     }
 
     public function viewAny(User $user) : bool
     {
-        return true;
+        return $user->canAny([
+            'view warboards', 
+            'view own company warboards', 
+            'view own faction company warboards'
+        ]);
     }
 
     public function view(User $user, WarBoard $warBoard) : bool
     {
-        return true;
+        return (
+            (
+                $user->can('view own company warboards') 
+                &&
+                $user->company()->id == $warBoard->company->id
+            )
+            ||
+            (
+                $user->can('view own faction warboards') 
+                &&
+                $user->faction()->id == $warBoard->company->faction->id
+            )
+        );
     }
 
     public function create(User $user) : bool
     {
-        return true;
+        return $user->canAny([
+            'create warboards', 
+            'create own company warboards', 
+            'create own faction warboards'
+        ]);
     }
 
     public function update(User $user, \App\Models\Events\WarBoard $warBoard) : bool
     {
-        return true;
+        return (
+            $user->can('edit warboards') 
+            || ( 
+            !empty($user->company())
+                &&
+                (
+                    (
+                        $user->can('edit own company warboards') 
+                        &&
+                        $user->company()->id == $warBoard->company->id 
+                    )
+                    ||
+                    (
+                        $user->can('edit own faction warboards') 
+                        &&
+                        $user->faction()->id == $warBoard->company->faction->id
+                    )
+                )
+            )
+        );
     }
 
     public function delete(User $user, WarBoard $warBoard) : bool
     {
-        return true;
+        return (
+            $user->can('delete warboards') 
+            || ( 
+            !empty($user->company())
+                &&
+                (
+                    (
+                        $user->can('delete own company warboards') 
+                        &&
+                        $user->company()->id == $warBoard->company->id 
+                    )
+                    ||
+                    (
+                        $user->can('delete own faction warboards') 
+                        &&
+                        $user->faction()->id == $warBoard->company->faction->id
+                    )
+                )
+            )
+        );
     }
 
     public function restore(User $user, \App\Models\Events\WarBoard $warBoard) : bool
     {
-        return true;
+        return $this->delete($user, $warBoard->company);
     }
 
     public function forceDelete(User $user, \App\Models\Events\WarBoard $warBoard) : bool
     {
-        return true;
+        return false;
     }
 }
